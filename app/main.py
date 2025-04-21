3c10bf8f5981175dc31a9bd738c8ef52be227b5b
sirococoa <torakia.mcml178@gmail.com>
2025-04-21 12:37:15 +0000

    Add Character

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 import pyxel
 
+from enum import Enum
+
 import pendulum
 
 WINDOW_W = 160
 WINDOW_H = 120
+
+FLOAR = WINDOW_H // 4 * 3
 
 TIME_DURATION = 30  # Time duration in seconds
 SIMURATION_FLAME = 300
@@ -84,6 +88,127 @@ class Pendulum:
         if i % 4 == 0:
             AffterImage.add_circle(*pos[-1])
 
+class CharacterStatus(Enum):
+    NONE = 0
+    JUMP = 1
+
+class Character:
+    W = 10
+    H = 10
+    MAX_X_VELOCITY = 2
+    MAX_Y_VELOCITY = 8
+    SPEED = 1
+    JUMP = 6
+
+    JUMP_COUNT = 5
+    JUMP_CHARGE_COUNT = 2
+
+    COLOR = 10
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.vx = 0
+        self.vy = 0
+        self.count = 0
+        self.status = CharacterStatus.NONE
+        self.double_jumped = False
+
+    def update(self):
+        if self.count > 0:
+            self.count -= 1
+        if self.count == 0:
+            self.status = CharacterStatus.NONE
+        if self.on_floor():
+            self.double_jumped = False
+
+        self.x += self.vx
+        self.y += self.vy
+        self.x_limit()
+
+        match self.status:
+            case CharacterStatus.NONE:
+                self.update_none()
+            case CharacterStatus.JUMP:
+                self.update_jump()
+
+    def update_none(self):
+        self.gravity()
+        self.move_x()
+        self.speed_limit()
+        if self.pushed_jump_button():
+            if self.on_floor():
+                self.vy = -self.JUMP
+                self.status = CharacterStatus.JUMP
+                self.count = self.JUMP_COUNT
+            elif not self.double_jumped:
+                self.vy = -self.JUMP
+                self.status = CharacterStatus.JUMP
+                self.count = self.JUMP_COUNT
+                self.double_jumped = True
+
+    def update_jump(self):
+        self.gravity()
+        self.move_x()
+        self.speed_limit()
+
+    def on_floor(self):
+        if self.y + self.H >= FLOAR:
+            return True
+        return False
+
+    # Applies gravity to the character.
+    def gravity(self):
+        if self.on_floor():
+            self.vy = 0
+            self.y = FLOAR - self.H
+        else:
+            self.vy += 1
+
+    # Moves the character left or right based on button presses.
+    def move_x(self):
+        vx = 0
+        if self.pushed_right_button():
+            vx += self.SPEED
+        if self.pushed_left_button():
+            vx -= self.SPEED
+        if vx == 0:
+            if self.vx > 0:
+                self.vx -= 1
+            elif self.vx < 0:
+                self.vx += 1
+        else:
+            self.vx += vx
+
+    # Clamps the horizontal (vx) and vertical (vy) velocities to ensure they
+    # do not exceed the defined maximum limits for smooth character movement.
+    def speed_limit(self):
+        self.vx = min(max(self.vx, -self.MAX_X_VELOCITY), self.MAX_X_VELOCITY)
+        self.vy = min(max(self.vy, -self.MAX_Y_VELOCITY), self.MAX_Y_VELOCITY)
+
+    def x_limit(self):
+        self.x = min(max(self.x, 0), WINDOW_W - self.W)
+
+    # Checks if the jump button is pressed (W, SPACE, or UP arrow keys)
+    def pushed_jump_button(self):
+        return pyxel.btn(pyxel.KEY_W) or pyxel.btn(pyxel.KEY_SPACE) or pyxel.btn(pyxel.KEY_UP)
+
+    # Checks if the left button is pressed (A or LEFT arrow keys)
+    def pushed_left_button(self):
+        return pyxel.btn(pyxel.KEY_A) or pyxel.btn(pyxel.KEY_LEFT)
+
+    # Checks if the right button is pressed (D or RIGHT arrow keys)
+    def pushed_right_button(self):
+        return pyxel.btn(pyxel.KEY_D) or pyxel.btn(pyxel.KEY_RIGHT)
+
+    def draw(self):
+        dx, dy = 0, 0
+        if self.status == CharacterStatus.JUMP:
+            if self.JUMP - self.count < self.JUMP_CHARGE_COUNT:
+                dx, dy = -1, 1
+            elif self.JUMP - self.count > self.JUMP_CHARGE_COUNT:
+                dx, dy = 1, -1
+        pyxel.rect(self.x + dx, self.y + dy, self.W - 2*dx, self.H - 2*dy, self.COLOR)
 
 class App:
     def __init__(self):
@@ -92,6 +217,7 @@ class App:
             pyxel.colors[i] = c
 
         self.pendulums = [Pendulum(3)]
+        self.character = Character()
         self.count = 0
         pyxel.run(self.update, self.draw)
 
@@ -99,6 +225,7 @@ class App:
         self.count += 1
         if self.count >= SIMURATION_FLAME:
             self.count = 0
+        self.character.update()
         AffterImage.update()
 
     def draw(self):
@@ -117,5 +244,9 @@ class App:
             pendulum.draw(self.count, reverse=True)
         AffterImage.draw(reverse=True)
 
+        pyxel.clip()
+        self.character.draw()
+
+        pyxel.line(0, FLOAR, WINDOW_W, FLOAR, 9)
 
 App()
